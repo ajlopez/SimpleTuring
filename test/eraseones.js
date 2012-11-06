06e779ef388b344427586d5386e8a9bf74afc031
ajlopez <webmaster@ajlopez.com>
2012-11-06 19:27:13 -0300

    Third parameter in run, specifying default value for empty cells

--- test/eraseones.js
+++ test/eraseones.js
@@ -21,3 +21,13 @@ assert.ok(result);
 assert.ok(Array.isArray(result));
 assert.equal(result.length, 6);
 assert.deepEqual(result, [0,0,0,0,0,0]);
+
+// second parameter: initial position in tape
+// third parameter: default value for unspecified cells
+
+var result = machine.run([0, 0, 1, 1, 1], 0, 0);
+
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.equal(result.length, 6);
+assert.deepEqual(result, [0,0,0,0,0,0]);
